7213b3d340d2e32fbb6dd7399daaf6edd2a5b2d4
Antony <antony.m.santana@aluno.senai.br>
2026-09-04 16:52:02 -0300

    commit nsei
    to nessa parte: 5. Galeria de Projetos com Filtro por Categoria (JavaScript):
    ○ Criar uma nova seção de galeria com botões de filtro no topo (ex: Todos,
    Romances, Biografia, Poesia). Ao clicar em um botão, apenas os cards
    daquela categoria devem ser exibidos via JavaScript. não terminei ainda

---
+++ js/js.js
@@ -0,0 +1,28 @@
+// 1. Guardamos os elementos da tela dentro de variáveis
+const modal = document.getElementById('modalContato')
+const btnAbrir = document.getElementById('btn')
+const btnFechar = document.getElementById('btnClose')
+const enviar = document.getElementById('enviar')
+const rolar = document.getElementById('btnTopo')
+
+
+btnAbrir.onclick = function() {
+    modal.classList.add('active')
+}
+
+btnFechar.onclick = function() {
+    modal.classList.remove('active')
+}
+
+enviar.onclick = function(){
+    window.alert('enviado com sucesso')
+    modal.classList.remove('active')
+}
+
+rolar.onclick = function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
